4df72beb1dfdf0c3406f2b482de75e511b969b4c
Renan Rodrigo <renanrb@ime.usp.br>
2018-10-04 21:33:47 -0300

    Adicionar exemplo com escopos

---
+++ escopo.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = 5;
+if(true){
+    let c;
+    let b = 8;
+    console.log(a);
+    console.log(b);
+    c = b;
+    let k = 7;
+    if(true){
+        let c = 9;
+        console.log(b);
+        console.log(a);
+        k = 177;
+    }
+    a = 20;
+    console.log(c);
+    console.log(k);
+}
+console.log(a);
+console.log(b);
+console.log(k);
